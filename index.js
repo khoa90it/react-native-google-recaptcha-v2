ad7620ce0eca1ca17f0a252bce81a89dd77c3e06
RooneyKhoaTran <khoadangtran.it@gmail.com>
2020-10-06 13:25:25 +0700

    rollback code

--- index.js
+++ index.js
@@ -48,7 +48,7 @@ class ConfirmGoogleCaptcha extends Component {
 const styles = StyleSheet.create({
     text: { fontSize: 15, fontWeight: 'bold', color: '#fff', textAlign: 'center', marginTop: 10 },
     modal: { margin: 0 },
-    wrapper: { flex: 1, backgroundColor: 'rgba(0, 0, 0, 0.3)', justifyContent: 'center', overflow: 'hidden', alignItems: 'center' }
+    wrapper: { flex: 1, backgroundColor: 'rgba(0, 0, 0, 0.3)', justifyContent: 'center', overflow: 'hidden'}
 });
 ConfirmGoogleCaptcha.propTypes = {
     siteKey: PropTypes.string.isRequired,
